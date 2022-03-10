f7bf654ca56c6ab5387eb4a159bc479a47adf017
yaredtt <yaredtekle22@gmail.com>
2022-03-10 10:13:50 -0800

    444

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-  char str1[] = "Programming is like building a multilingual puzzle";
-  puts(str1);
+ puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
 
