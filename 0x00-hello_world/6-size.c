ccb90efa0626f0985eac37a838ed8ac04bddc092
yaredtt <yaredtekle22@gmail.com>
2022-03-10 13:49:13 -0800

    add teo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - print the string in the put function
+ * main - Entry point
  *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return(0);
-}
+	char c;
+	int i;
+	long li;
+	float f;
 
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
