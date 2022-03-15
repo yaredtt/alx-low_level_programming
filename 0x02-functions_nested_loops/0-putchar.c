5ca11f5f4b9720859132141f010000c4a9abf1f2
yaredtt <yaredtekle22@gmail.com>
2022-03-15 04:58:11 -0700

    putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *s = "_putchar";
+
+	while (*s != '\0')
+{
+	_putchar(*s);
+	s++;
+}
+	_putchar('\n');
+	return (0);
+}
