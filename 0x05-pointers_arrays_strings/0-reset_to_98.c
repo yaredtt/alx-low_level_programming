e377d851791d68c3cd0d6cdefd2cc71c54a7de4c
yaredtt <yaredtekle22@gmail.com>
2022-03-21 13:00:58 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	void reset_to_98
+{
+
+	int *ptr;
+
+	*ptr = 98;
+}
+	return (0);
+}
