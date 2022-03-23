d035a2f93b09610ee8ee4497b850ab7c68ad07e9
yaredtt <yaredtekle22@gmail.com>
2022-03-22 18:34:39 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - print string newline
+ * @str:string output
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a])
+{
+	_putchar(str[a]);
+	a++;
+}
+	_putchar("\n");
+}
