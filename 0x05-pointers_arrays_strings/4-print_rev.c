87aecb06cca5991cbbd5af4ab85ef4bd202539bc
yaredtt <yaredtekle22@gmail.com>
2022-03-22 19:18:13 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - print string
+ * @s: string pointer
+ * Return:
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[x])
+	x++;
+	while (x--)
+	{
+	_putchar(s[x]);
+	}
+	_putchar("\n");
+}
