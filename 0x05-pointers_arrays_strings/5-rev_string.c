4fcc46feb9253b5fcfd5d02adfcab66a81c47efb
yaredtt <yaredtekle22@gmail.com>
2022-03-22 19:43:16 -0700

    ggg

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: string char
+ * Return: voi
+ */
+void rev_string(char *s)
+{
+	int x = 0;
+
+	while (s[x])
+	x++;
+	while (s[x])
+{
+	x--;
+	_putchar('\n');
+}
+}
