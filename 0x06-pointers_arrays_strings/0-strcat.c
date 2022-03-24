5b2385eee75c98e5c06fc923b4c6c0a2f6d0adb8
yaredtt <yaredtekle22@gmail.com>
2022-03-24 02:22:13 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcat - concatinate two strings
+ * @d: destination string
+ * @s: source string
+ * Return:
+ */
+char *_strcat(char *dest, char *src)
+{
+	char str1[];
+	char str2[];
+	int len;
+	int i;
+
+	strcat(str1, str2);
+	len = strlen(str1);
+	while (i <= len)
+{
+	_putchar(str1[]);
+	i++;
+}
+	_putchar('\n');
+}
