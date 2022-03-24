2277cbaa1a8b66c2fa37af3d33db86c7ff3da5b2
yaredtt <yaredtekle22@gmail.com>
2022-03-24 04:10:28 -0700

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ */
+	int main(void)
+{
+	int sum = 2772;
+	int n;
+
+	srand(time(0));
+	while (sum)
+	{
+	n = rand() % 100;
+	if (n <= 94)
+	n += 32;
+	else
+	continue;
+	if (sum - n == 0)
+	{
+	sum -= n;
+	printf("%c", n);
+	}
+	else if (sum - n - 32 > 0)
+	{
+	sum -= n;
+	printf("%c", n);
+	}
+	}
+	return (0);
+}
