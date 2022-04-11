4fdf3e0a4ef4eb54e3b5e901761308600a2d9336
yaredtt <yaredtekle22@gmail.com>
2022-04-11 14:03:49 -0700

    dog

--- 0x0E-Structures_typedef/0-main.c
+++ 0x0E-Structures_typedef/0-main.c
@@ -6,13 +6,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
+	int main(void)
 {
-    struct dog my_dog;
+	struct dog my_dog;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
 }
--- 0x0E-Structures_typedef/2-main.c
+++ 0x0E-Structures_typedef/2-main.c
@@ -6,13 +6,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
+	int main(void)
 {
-    struct dog my_dog;
+	struct dog my_dog;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    print_dog(&my_dog);
-    return (0);
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	print_dog(&my_dog);
+	return (0);
 }
